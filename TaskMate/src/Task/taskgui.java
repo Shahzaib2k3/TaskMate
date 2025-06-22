8ac336c9113a1ca508d31f994ce0fd7183c6c0fb
shahzaib <shahzaibutt2003@gmail.com>
2025-06-23 00:53:46 +0500

    Day 1 GUI layout complete with JTable and components

--- TaskMate/src/Task/taskgui.java
+++ TaskMate/src/Task/taskgui.java
@@ -3,10 +3,19 @@ package Task;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class taskgui {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -36,9 +45,49 @@ public class taskgui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 533, 509);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Task Title");
+		lblNewLabel.setBounds(10, 42, 66, 13);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(69, 39, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Due Date");
+		lblNewLabel_1.setBounds(10, 87, 70, 13);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(69, 84, 96, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add Task");
+		btnNewButton.setBounds(355, 38, 136, 21);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Mark Completed");
+		btnNewButton_1.setBounds(355, 83, 136, 21);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Delete Task");
+		btnNewButton_2.setBounds(355, 127, 136, 21);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Title", "Date", "Completed"
+			}
+		));
+		table.setBounds(10, 227, 499, 235);
+		frame.getContentPane().add(table);
 	}
-
 }
